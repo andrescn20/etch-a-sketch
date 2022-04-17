a8f3cff587a70d2eed05ad79ca49023480da24f9
Andres <andrescn20@gmail.com>
2022-04-17 12:37:14 -0600

    Rearrange grid to input value with button

--- script.js
+++ script.js
@@ -1,68 +1,77 @@
 const mainContainer = document.querySelector ('.mainContainer');
 const controls = document.querySelector ('.controls');
 const array = []; 
+const generatorBtn = document.getElementById ('generatorBtn');
+const sizeInput = document.getElementById ('size')
+
+
 
 let containerSide = 768;
 let squareSide = 0; 
-const squaresArray = [];
+let gridStatus= false; 
+let lastSize = 0; 
+let currentSize = 0;
 
-function calculateDimension(a){
-    squareSide = (containerSide/a)-2;
+generatorBtn.onclick = mainFunction; 
+
+function mainFunction (){
+currentSize = sizeInput.value; 
+generator (currentSize);
+
+}
+
+function generator(gridSide){
+    
+    if (gridStatus === false){
+
+        createSquares(gridSide);
+
+    }else {
+
+    eraseSquares(lastSize);
+    createSquares(gridSide);
+    }
+
+
+}
+function calculateDimension(gridSide){
+    squareSide = (containerSide/gridSide)-2;
     return squareSide+'px';
 }
 
-function createSquares(a){
-    total = a*a;
+function eraseSquares(gridSide) {
+
+    total = gridSide*gridSide;
+    for ( i = 0 ; i < total ; i ++){
+        const square = document.querySelector('.square');
+        square.remove();
+    }
+
+
+}
+function createSquares(gridSide){
+
+    total = gridSide*gridSide;
     for ( i = 0 ; i < total ; i ++){
         const square = document.createElement ('div');
         square.setAttribute('class' , 'square');
-        square.style.height = calculateDimension(a);
-        square.style.width = calculateDimension(a);
+        square.style.height = calculateDimension(gridSide);
+        square.style.width = calculateDimension(gridSide);
         mainContainer.appendChild(square); 
-        squaresArray.push(square);
     }
+
+    gridStatus = true; 
+    lastSize = gridSide;
 
     const squares = document.querySelectorAll('.square');
 
     squares.forEach ((square) => {
 
-    square.onclick = changeColor; 
+    square.onmouseenter= changeColor; 
 
     });
 }
-
-
 function changeColor(e) {
     e.target.style.background = 'black';
 }
-
-
-/* This code is working correctly: 
-
-const btn = document.querySelectorAll('button');
-
-btn.forEach ((button) => {
-
-    button.onclick = changeColor; 
-
-}); */
-
-
-/*const blackSquare = mainContainer.getElementsByClassName("square");
-    let length = blackSquare.length; 
-    for (i = 0 ; i < length ; i++){
-    
-        blackSquare[i].addEventListener("onmouseover", (i)=> {
-            blackSquare[i].style.backgroundColor = 'black';
-        });
-
-function borderStatus(){
-    let border = document.getElementById("borderBtn");
-    border.classList.toggle("border");
-}
-
-function colorStatus(){
-    let border = document.getElementsByClassName('.square');
-    border.classList.toggle("color");
-}*/
 
