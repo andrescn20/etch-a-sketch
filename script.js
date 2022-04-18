cbc01368f8368df6ccdb4b17078b9839fd9999ee
Andres <andrescn20@gmail.com>
2022-04-17 23:34:21 -0600

    Add Eraser Function

--- script.js
+++ script.js
@@ -4,19 +4,22 @@ const array = [];
 const generatorBtn = document.getElementById ('generatorBtn');
 const sizeInput = document.getElementById ('size');
 const colorBtn = document.querySelector('.colorBtn');
+const eraserBtn = document.querySelector('.eraserBtn');
 
-let containerSide = 768;
+let containerSide = 520;
 let squareSide = 0; 
 let gridStatus= false; 
 let lastSize = 0; 
 let currentSize = 0;
 let black = true; 
+let white = false; 
 
 
-window.onload = generator(64);
+window.onload = generator(24);
 
 
 generatorBtn.onclick = mainFunction; 
+
 colorBtn.onclick = () => {
     switch (black){
         case true: black = false; 
@@ -27,6 +30,17 @@ colorBtn.onclick = () => {
                     break;
     }
 }
+eraserBtn.onclick = () => {
+    switch (white){
+        case true: white = false; 
+            eraserBtn.classList.toggle('eraser');
+            break; 
+        case false: white = true; 
+            eraserBtn.classList.toggle('eraser');
+             break;
+    }
+}
+
 
 function mainFunction (){
     let input = sizeInput.value;
@@ -82,26 +96,22 @@ function createSquares(gridSide){
 
     const squares = document.querySelectorAll('.square');
     squares.forEach ((square) => {
-    square.onmouseenter= changeColor; 
+    square.onmousemove= changeColor; 
     });
 
 
 }
 function changeColor(e) {
 
-    switch(black){
 
-        case true: 
+    if (black === true && white === false) { 
             e.target.style.background = 'black';
-            break;
-        
-        case false: 
+
+    } else if (black === false && white === false){ 
             let randomColor = Math.floor(Math.random()*16777215).toString(16);
             e.target.style.background = "#" + randomColor;
-            break;
-        default:
-            e.target.style.background = 'white';
-            
+    }else{
+            e.target.style.background = 'white'; 
     }
 }
 
